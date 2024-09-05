36e53af0b33eb1404e344b15d7a5690e1e31ad58
MrPenguz <mohammadmasere@gmail.com>
2024-09-05 09:46:29 +0300

    first commit

---
+++ src/components/Generator.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SectionWrapper } from './SectionWrapper'
+export default function Generator() {
+    return (
+        <SectionWrapper header={"generate your workout"} title={["It\'s", "Huge", "o\'clock"]} >
+            dengo
+        </SectionWrapper>
+    )
+}
